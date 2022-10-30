2e715dee79176ef778b24edd48d7a11905ddf269
Stefan Exner <stex@stex.codes>
2022-10-30 18:43:50 +0100

    at least it finds the cypress executable now, no idea why it ignores the entrypoint thoug...
    Signed-off-by: Stefan Exner <stex@stex.codes>

--- docker-entrypoint.dev.sh
+++ docker-entrypoint.dev.sh
@@ -4,6 +4,9 @@ set +e
 # This is a workaround to be able to run a custom command in github actions
 # when we spin up a backend for cypress tests...
 # I wish there was a way to set the service command like in gitlab CI.
-[[ -n "$CYPRESS" ]] && bundle exec rake db:setup
-
-exec "$@"
+if [[ -n "$CYPRESS" ]]; then
+  bundle exec rake db:setup
+  bundle exec rails s -b 0.0.0.0
+else
+  exec "$@"
+fi
